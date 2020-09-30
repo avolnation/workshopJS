8dba7da1fcdf9f52b78940bbc2e0b1389cf2fa3a
avolnation <avolnationmobsys@gmail.com>
2020-09-30 15:10:49 +0300

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,51 @@
+'use strict';
+//day 1
+
+//получаем элементы
+
+const btnBurger = document.querySelector('.btn-burger');
+const catalog = document.querySelector('.catalog');
+const btnClose = document.querySelector('.btn-close');
+//const catalogList = document.querySelector('.catalog-list');
+const subCatalog = document.querySelector('.subcatalog');
+const subCatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+
+//открываем менюшку
+    const openMenu = () => {
+        catalog.classList.add('open');
+        overlay.classList.add('active');
+}
+//закрываем менюшку
+    const closeMenu = () => {
+        catalog.classList.remove('open');
+        overlay.classList.remove('active');
+        closeSubMenu();
+}
+    const openSubMenu = event => {
+        event.preventDefault();
+        const itemList = event.target.closest('.catalog-list__item');
+        if(itemList){
+            subCatalogHeader.innerHTML = itemList.innerHTML;
+            subCatalog.classList.add('subopen');
+        }
+}
+
+    const closeSubMenu = () => {
+        subCatalog.classList.remove('subopen');
+    }
+//получаем клик
+    btnBurger.addEventListener('click', openMenu);
+    btnClose.addEventListener('click', closeMenu);
+    overlay.addEventListener('click', closeMenu);
+    /*document.addEventListener('keydown', (event) => 
+    {if(event.code === 'Escape')
+        {closeMenu()}
+    });*/
+    catalog.addEventListener('click', openSubMenu);
+    btnReturn.addEventListener('click', closeSubMenu);
